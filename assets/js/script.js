7185cdcf11310605831585291991f171b704a2ab
Daniel Miranda <Dm2639532@gmail.com>
2023-08-30 15:22:12 -0300

    To-do List

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var anotar = document.getElementById('anotar');
+var salvar = document.getElementById('salvar');
+var notas = document.getElementById('notas');
+var formulario = document.querySelector('.add_nota');
+
+
+
+function verificarAnotação (){
+    if(anotar.value.trim() != 0){
+        let notaNova = document.createElement('article');
+
+        let check = document.createElement('input');
+        check.type = 'checkbox';
+        check.className = 'checked';
+
+        let textoConteudo = document.createElement('p');
+        textoConteudo.innerText = anotar.value;
+
+        let deletar = document.createElement('button');
+        deletar.className = 'deletar';
+        deletar.innerText = 'X';
+
+
+        notaNova.appendChild(check)
+        notaNova.appendChild(textoConteudo)
+        notaNova.appendChild(deletar)
+        notas.appendChild(notaNova)
+
+        // Remover nota
+        deletar.addEventListener('click', () => {
+            notaNova.remove();
+        });
+    }
+}
+
+function salvarNota(e) {
+    e.preventDefault();
+    verificarAnotação();
+    formulario.reset()
+};
+
+
+salvar.addEventListener('click', salvarNota);
+
+salvar.addEventListener('dblclick', () => {
+    console.log(deletar)
+})
